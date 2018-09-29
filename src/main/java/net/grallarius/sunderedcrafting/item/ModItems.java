4ecb7d3076ea2ea8c629435ebef5840f9db1e364
Liruilos <lolliewalton@gmail.com>
2018-09-29 20:09:58 +0200

    started working on advancement system

--- src/main/java/net/grallarius/sunderedcrafting/item/ModItems.java
+++ src/main/java/net/grallarius/sunderedcrafting/item/ModItems.java
@@ -5,21 +5,15 @@ import net.grallarius.sunderedcrafting.item.tool.ItemModSword;
 
 public class ModItems {
 
-    public static ItemOreChunk ingotCopper = new ItemOreChunk("ingot_copper", "ingotCopper");
-//    public static ItemModSword copperSword = new ItemModSword(SunderedCrafting.copperToolMaterial, "copper_sword");
+    public static ItemOreChunk ingotCopper = new ItemOreChunk("ore_chunk_copper", "ingotCopper");
+    public static ItemModSword copperSword = new ItemModSword(SunderedCrafting.copperToolMaterial, "copper_sword");
 
-    public static ItemBase ingotWobble = new ItemBase("wobble");
 
 
     public static void preInit() {
                 ingotCopper.register();
-                ingotWobble.register();
-//                copperSword.register();
+                copperSword.register();
     }
 
-//    public static void registerModels() {
-//        ingotCopper.register();
-//        copperSword.register();
-//        ingotWobble.register();
-//    }
+
 }
